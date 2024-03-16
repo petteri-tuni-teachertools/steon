c2f3609ab7022848d821148767cce664d59281be
Petteri Jekunen <petteri.jekunen@tuni.fi>
2024-03-16 19:00:47 +0000

    Added third ray script

--- stec23-copy-ray.sh
+++ stec23-copy-ray.sh
@@ -7,12 +7,20 @@ IP_LIST=data-local/stec23-ip-latest
 SSH_KEY_SAM22=/var/lib/centreon-engine/.ssh/sam22_key
 RAY_SCRIPT_1=monitor-scripts/ray.sh
 RAY_SCRIPT_2=monitor-scripts/ray2.sh
+RAY_SCRIPT_3=monitor-scripts/ray3.sh
+TIMEOUT=5
+OPTION="-o StrictHostKeyChecking=no"
 
 echo First script: $RAY_SCRIPT_1
 echo
-for ip in `cat $IP_LIST`; do echo $ip; sudo timeout 2 scp -i $SSH_KEY_SAM22 $RAY_SCRIPT_1 $SSH_USER@$ip:. ; done
+for ip in `cat $IP_LIST`; do echo $ip; sudo timeout $TIMEOUT scp $OPTION -i $SSH_KEY_SAM22 $RAY_SCRIPT_1 $SSH_USER@$ip:. ; done
 echo
 echo 2nd script: $RAY_SCRIPT_2
 echo
-for ip in `cat $IP_LIST`; do echo $ip; sudo timeout 2 scp -i $SSH_KEY_SAM22 $RAY_SCRIPT_2 $SSH_USER@$ip:. ; done
+for ip in `cat $IP_LIST`; do echo $ip; sudo timeout $TIMEOUT scp $OPTION -i $SSH_KEY_SAM22 $RAY_SCRIPT_2 $SSH_USER@$ip:. ; done
+
+echo
+echo 3rd script: $RAY_SCRIPT_3
+echo
+for ip in `cat $IP_LIST`; do echo $ip; sudo timeout $TIMEOUT scp $OPTION -i $SSH_KEY_SAM22 $RAY_SCRIPT_3 $SSH_USER@$ip:. ; done
 
